9a5d2af00d450c622f679af6409b9859926a0923
konradmh3 <konradm.h.3@gmail.com>
2023-05-17 09:49:31 -0700

    idk

--- my-react-website/src/pages/AboutMe.js
+++ my-react-website/src/pages/AboutMe.js
@@ -1,8 +1,16 @@
 import "../style/Page.css";
+// to import an exponent, from this path: my-react-website/src/pages/AboutMeContent.js
+import AboutMeContent from "./AboutMeContent.js";
+
 
 const AboutMe = () => {
-    return <h1>About Me</h1>;
+    return (
+    <>
+      <h1>About Me</h1>
+      <AboutMeContent />
+    </>
+    );
   };
   
-  export default AboutMe;
+export default AboutMe;
   
---
+++ my-react-website/src/pages/AboutMeContent.js
@@ -0,0 +1,14 @@
+import "../style/AboutMeContent.css";
+
+
+const AboutMeContent = () => {
+    return (
+    <>
+    <div className="contentContainer">
+            <p>Test</p>
+    </div>
+    </>
+    );
+  };
+  
+export default AboutMeContent;
