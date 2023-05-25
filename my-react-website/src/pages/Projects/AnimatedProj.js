01405ec443ad5312f7f282547a2707ad019a5c2a
konradmh3 <konradm.h.3@gmail.com>
2023-05-25 13:25:56 -0700

    added more to the layout of each project, added prop for positioning of project.

--- my-react-website/src/pages/Projects/AnimatedProj.js
+++ my-react-website/src/pages/Projects/AnimatedProj.js
@@ -2,22 +2,74 @@ import "../../style/Projects.css";
 import { motion } from "framer-motion";
 
 const AnimatedProj = (props) => {
-  // add some code for when it renders the component it will change opacity to 1 like so:
-
-  return (
-    <>
-      <div className="projectContainer">
-        <h1>{props.projectName}</h1>
-
-        <motion.div id="parallelogram" initial={{x: "60%", skew: 10}} whileHover={{scale: 1.1, x: "0%", skew: 10 }}>
-          <div className="projectContentContainer">
-            <p>display is set to none for content container</p>
+  const projectPosition =
+    props.position === "right" ? (
+      <>
+        <motion.div
+          animate={{ opacity: [0, 1], transition: { delay: props.delay } }}
+          className="projectContainer"
+        >
+          <div className="projectTitle">
+            {/* insert an image here */}
+            <img
+              src={require("../../assets/" + props.projectName + ".png")}
+              alt={props.projectName}
+            />
           </div>
 
+          <div className="projectInfo">
+            <p>
+              Hello my name is konrad. blah blah blah blah blah i like to eat
+              food and type random stuff when i dont know what to type so i can
+              see how things will look when they are fully typedout and stuff
+              and yeah! This padding looks like it will hold well!
+            </p>
+          </div>
+          <div className="parallelogramContainer-right">
+            <motion.div
+              className="parallelogram"
+              initial={{ x: "0%", skew: 10 }}
+              whileHover={{ scale: 1.1, x: "-50%", skew: 10 }}
+            ></motion.div>
+          </div>
         </motion.div>
-      </div>
-    </>
-  );
+      </>
+    ) : (
+      <>
+        <motion.div
+          animate={{ opacity: [0, 1], transition: { delay: props.delay } }}
+          className="projectContainer"
+        >
+          <div className="projectTitle">
+            {/* insert an image here */}
+            <img
+              src={require("../../assets/" + props.projectName + ".png")}
+              alt={props.projectName}
+            />
+          </div>
+
+          <div className="parallelogramContainer-left">
+            <motion.div
+              className="parallelogram"
+              initial={{ x: "0%", skew: 10 }}
+              whileHover={{ scale: 1.1, x: "50%", skew: 10 }}
+            ></motion.div>
+          </div>
+          <div className="projectInfo">
+            <p>
+              Hello my name is konrad. blah blah blah blah blah i like to eat
+              food and type random stuff when i dont know what to type so i can
+              see how things will look when they are fully typedout and stuff
+              and yeah! This padding looks like it will hold well!
+            </p>
+          </div>
+        </motion.div>
+      </>
+    );
+    console.log(projectPosition);
+  // add some code for when it renders the component it will change opacity to 1 like so:
+
+  return projectPosition;
 };
 
 export default AnimatedProj;
--- my-react-website/src/pages/Projects/Projects.js
+++ my-react-website/src/pages/Projects/Projects.js
@@ -5,10 +5,8 @@ import Header from "../Templates/Header.js";
 const Projects = () => {
     return (<>
     <Header page = "Projects"/>
-    <AnimatedProj projectName = "FooDood" delay={.66}/>
-    <AnimatedProj projectName = "Home System" delay={.66}/>
-    <AnimatedProj projectName = "Stick Game" delay={.66}/>
-    <AnimatedProj projectName = "Key Logger" delay={.66}/>
+    <AnimatedProj projectName = "fooDoodTitle" delay={.99} position = "left"/>
+    <AnimatedProj projectName = "fooDoodTitle" delay={.99} position = "right"/>
     </>);
   };
   
