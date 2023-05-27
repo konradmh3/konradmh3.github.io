bdcc8225ca93f89bf14fabe6897d14949f107824
konradmh3 <konradm.h.3@gmail.com>
2023-05-26 22:05:39 -0700

    added project transition and buttons to scroll smoothly

--- my-react-website/src/pages/Projects/Projects.js
+++ my-react-website/src/pages/Projects/Projects.js
@@ -1,41 +1,88 @@
 import "../../style/Page.css";
-import AnimatedProj from "./AnimatedProj";
-import Header from "../Templates/Header.js";
 import ProjectsNew from "../ProjectsNew";
 import { useEffect, useState } from "react";
-import { motion, useUnmountEffect } from "framer-motion";
+import { motion } from "framer-motion";
 import { useRef } from "react";
 
 const Projects = () => {
-    const projectRefs = [useRef(), useRef(), useRef()];
-    var displayedPage = 0;
+  const projectRefs = [useRef(), useRef(), useRef()];
+  const [arrowUpDisplay, setArrowUpDisplay] = useState("none");
+  const [arrowDownDisplay, setArrowDownDisplay] = useState("block");
+  const [displayedPage, setDisplayedPage] = useState(0);
 
-    const executeScrollProject1 = () => {
-      projectRefs[0].current?.scrollIntoView({behavior: 'smooth'})
+  useEffect(() => {
+    projectRefs[displayedPage].current?.scrollIntoView({ behavior: "smooth" });
+    if (arrowUpDisplay === "block" && displayedPage === 0) {
+      setArrowUpDisplay("none");
+    }
+    if (arrowDownDisplay === "block" && displayedPage === 2) {
+      setArrowDownDisplay("none");
+    }
+    if (arrowDownDisplay === "none" && displayedPage < 2) {
+      setArrowDownDisplay("block");
+    }
+    if (arrowUpDisplay === "none" && displayedPage > 0) {
+      setArrowUpDisplay("block");
+    }
+  }, [displayedPage]);
 
-    };
-
-    const executeScrollProject2 = () => {
-      projectRefs[1].current?.scrollIntoView({behavior: 'smooth'})
-    };
-
-    const executeScrollProject3 = () => {
-      projectRefs[2].current?.scrollIntoView({behavior: 'smooth'})
-    };
-
-
-
-    return (
-    <div className="scrollContainer">
-      <motion.div whileHover={{scale: 1.1}} onClick={executeScrollProject1} className="staticScrollerProject1"></motion.div>
-      <motion.div whileHover={{scale: 1.1}} onClick={executeScrollProject2} className="staticScrollerProject2"></motion.div>
-      <motion.div whileHover={{scale: 1.1}} onClick={executeScrollProject3} className="staticScrollerProject3"></motion.div>
-      <ProjectsNew ref={projectRefs[0]} backgroundColor="lightblue"/>
-      <ProjectsNew ref={projectRefs[1]} backgroundColor="lightgreen"/>
-      <ProjectsNew ref={projectRefs[2]} backgroundColor="lightred"/>
-    </div>
-    );
+  const executeScrollUp = () => {
+    if (displayedPage > 0) {
+      setDisplayedPage(displayedPage - 1);
+    } else {
+      projectRefs[displayedPage].current?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
   };
-  
-  export default Projects;
-  
+
+  const executeScrollDown = () => {
+    // edit this when you add a project!
+    if (displayedPage < 2) {
+      setDisplayedPage(displayedPage + 1);
+      console.log(displayedPage);
+    } else {
+      projectRefs[displayedPage].current?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <div className="scrollContainer">
+      <motion.div
+        style={{ display: arrowUpDisplay }}
+        animate={{ opacity: [0, 1], rotate: [0, -90] }}
+        transition={{ duration: 1 }}
+        initial={{ x: -100, y: -100 }}
+        whileHover={{ scale: 1.1, x: -100, y: -100 }}
+        onClick={executeScrollUp}
+        className="staticScrollerUp"
+      >
+        <img
+          src={require("../../assets/" + "arrow(1)" + ".png")}
+          alt={"arrow"}
+        />
+      </motion.div>
+      <motion.div
+        style={{ display: arrowDownDisplay }}
+        animate={{ opacity: [0, 1], rotate: [0, 90] }}
+        transition={{ duration: 1 }}
+        initial={{ x: -100, y: -100 }}
+        whileHover={{ scale: 1.1, x: -100, y: -100 }}
+        onClick={executeScrollDown}
+        className="staticScrollerDown"
+      >
+        <img
+          src={require("../../assets/" + "arrow(1)" + ".png")}
+          alt={"arrow"}
+        />
+      </motion.div>
+      <ProjectsNew ref={projectRefs[0]} backgroundColor="lightblue" />
+      <ProjectsNew ref={projectRefs[1]} backgroundColor="lightgreen" />
+      <ProjectsNew ref={projectRefs[2]} backgroundColor="#ffb365" />
+    </div>
+  );
+};
+
+export default Projects;
