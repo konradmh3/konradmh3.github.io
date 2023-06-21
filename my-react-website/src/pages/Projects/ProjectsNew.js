e794f1588c9564e1da5fb3b264d3af7617543b74
konradmh3 <konradm.h.3@gmail.com>
2023-06-21 11:29:42 -0700

    add some whatever

--- my-react-website/src/pages/Projects/ProjectsNew.js
+++ my-react-website/src/pages/Projects/ProjectsNew.js
@@ -6,14 +6,10 @@ import FooDoodProject from "./FooDoodProject";
 
 const ProjectsNew = (props, ref) => {
   // lets make a variable conditional assignment to decide which project to display based on the project name prop
-  const project = props.projectName === "FooDood" ? <FooDoodProject position="right"/> : <></>;
+  const project1 = props.projectName === "FooDood" ? <FooDoodProject position="right"/> : <></>;
 return (<>
     <motion.div className="fatherContainer" ref={ref} style={{backgroundColor: props.backgroundColor}}>
-      {/* here i can shoose between different components i want to add to the projects page. */}
-      {/* then add all props needed to padd to that component + an additional prop to choose which to display like a boolean.
-          we can then take that boolean and use an if else to decide what to display. Example of if else in animatedProject.js
-      */}
-      {project}
+      {project1}
       
     </motion.div>
     </>);
