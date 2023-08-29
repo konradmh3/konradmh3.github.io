586c4b3e5959635800dce4c116f7eb3857aee1ff
konrad <konradm.h.3@gmail.com>
2023-08-28 20:57:53 -0700

    added code like colors for webdev gives a lot more flavor as well as changed all white to blur

--- my-react-website/src/pages/AboutMe/AboutMe.js
+++ my-react-website/src/pages/AboutMe/AboutMe.js
@@ -18,27 +18,27 @@ const AboutMe = () => {
     return (
     <>
       <div className="aboutMeTitleContainer">
-        <NewTextAnimation text="Hello! My name is Konrad!" />
+        <NewTextAnimation sth1tag={[<span style={{color:"grey"}}>&lt;</span>, <span style={{color: "rgb(21, 110, 173)"}}>h1</span>, <span style={{color:"grey"}}>&gt;</span>]} text="Hello! My name is Konrad!" endh1tag={[<span style={{color:"grey"}}>&lt;/</span>, <span style={{color: "rgb(21, 110, 173)"}}>h1</span>, <span style={{color:"grey"}}>&gt;</span>]} />
       </div>
 
       {/* lets wrap bottom in a motiondiv to delay opacity in case of closing menu when staying on the page and it appears over the menu */}
       
-      <CodeSubtitles subtitle='<AboutMe yeah="YourBoi">' />
+      <CodeSubtitles subtitle={[<span style={{color:"grey"}}>&lt;</span>, <span style={{color: "rgb(21, 110, 173)"}}>AboutMe</span>, ' ',  <span style={{color: "rgb(110, 188, 230)"}}>yeah</span>, '=', <span style={{color: "rgb(179, 127, 82)"}}>"YourBoi"</span>, <span style={{color:"grey"}}>&gt;</span>]} />
       <AboutMeDescription ref={aboutMeRefs[0]} title="Home Town" text="My home town is Long Beach, California! I was born here and have lived here for 23 years now!" />
       <AboutMeDescription ref={aboutMeRefs[1]} title="Schooling" text="I went to school at Long Beach State to recieve my Bachelors in Computer Science as well as a minor in Mathematics." />
       <AboutMeDescription ref={aboutMeRefs[2]} title="Extracurriculars" text="I like doing things and other things and boom! More things!" />
-      <CodeSubtitles subtitle='</AboutMe>' />
+      <CodeSubtitles subtitle={[<span style={{color:"grey"}}>&lt;/</span>, <span style={{color: "rgb(21, 110, 173)"}}>AboutMe</span>, <span style={{color:"grey"}}>&gt;</span>]} />
 
       {/* Next section on about me should be about */}
-      <CodeSubtitles subtitle='<Grind projects="Mid">' />      
+      <CodeSubtitles subtitle={[<span style={{color:"grey"}}>&lt;</span>, <span style={{color: "rgb(21, 110, 173)"}}>Grind</span>, ' ',  <span style={{color: "rgb(110, 188, 230)"}}>projects</span>, '=', <span style={{color: "rgb(179, 127, 82)"}}>"Mid"</span>, <span style={{color:"grey"}}>&gt;</span>]} />
       <AboutMeDescription ref={aboutMeRefs[3]} title="FoodDood" text="FooDood is a mobile app built with react native to help local businesses grow and consumers eat!"/>
       <AboutMeDescription ref={aboutMeRefs[4]} title="This Website!" text="My personal website is built with react, framer motion for animation!" />
       <AboutMeDescription ref={aboutMeRefs[5]} title="Stick" text="Stick is a 2D platformer built with unity!" />
-      <CodeSubtitles subtitle='</Grind>' />
+      <CodeSubtitles subtitle={[<span style={{color:"grey"}}>&lt;/</span>, <span style={{color: "rgb(21, 110, 173)"}}>Grind</span>, <span style={{color:"grey"}}>&gt;</span>]} />
 
-      <CodeSubtitles subtitle='<Grind work="Experience">' />      
+      <CodeSubtitles subtitle={[<span style={{color:"grey"}}>&lt;</span>, <span style={{color: "rgb(21, 110, 173)"}}>Grind</span>, ' ',  <span style={{color: "rgb(110, 188, 230)"}}>work</span>, '=', <span style={{color: "rgb(179, 127, 82)"}}>"Experience"</span>, <span style={{color:"grey"}}>&gt;</span>]} />
       <AboutMeDescription ref={aboutMeRefs[6]} title="ASI" text="Associated Students Inc. for one year."/>
-      <CodeSubtitles subtitle='</Grind>' />
+      <CodeSubtitles subtitle={[<span style={{color:"grey"}}>&lt;/</span>, <span style={{color: "rgb(21, 110, 173)"}}>Grind</span>, <span style={{color:"grey"}}>&gt;</span>]} />
 
     </>
     );
--- my-react-website/src/pages/AboutMe/NewTextAnimation.js
+++ my-react-website/src/pages/AboutMe/NewTextAnimation.js
@@ -1,12 +1,14 @@
 // This component will take in a string as an argument and animate it using typewriter effect api
-
+import "../../style/AboutMe.css";
 import Typewriter from 'typewriter-effect';
 import React from 'react';
 import {motion} from 'framer-motion';
 
-const NewTextAnimation = ({ text }) => {
+const NewTextAnimation = ({ sth1tag, text , endh1tag}) => {
+
     return (
-        <motion.div animate={{ opacity: [0, 1], transition: {delay: 1} }}>
+        <motion.div style={{  display: "flex", flexDirection: "row"}} animate={{ opacity: [0, 1], transition: {delay: 1} }}>
+        {sth1tag}
         <Typewriter
         options={{
             strings: text,
@@ -14,6 +16,7 @@ const NewTextAnimation = ({ text }) => {
             loop: true,
         }}
         />
+        {endh1tag}
         </motion.div>
     );
 };
