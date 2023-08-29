757650ee29e1dd5f4b3937bbc72aa6c87cfd7e0e
konrad <konradm.h.3@gmail.com>
2023-08-29 01:04:20 -0700

    quick fix

--- my-react-website/src/pages/AboutMe/AboutMeDescription.js
+++ my-react-website/src/pages/AboutMe/AboutMeDescription.js
@@ -2,7 +2,7 @@
 // it will take in a subtitle and short desription as props
 import React from "react";
 import "../../style/AboutMe.css";
-import { spring, useInView } from "framer-motion";
+import { useInView } from "framer-motion";
 import { forwardRef } from "react";
 
 
@@ -10,22 +10,30 @@ import { motion } from "framer-motion";
 
 const AboutMeDescription = (props, ref) => {
   const isInView = useInView(ref, { once: true, amount: 1 });
+  
   var count = 0;
 
 
   
-// create more info function to handle the animation of the div
+// create more info function to handle the animation of the div    
+// count to see how many times the div has been clicked for testing
   const moreInfo = () => {
     count++;
     console.log("This button clicked", count, "times!");
   };
 
 
+
+
   return (
     <div className="aboutMeDescriptionContainer">
-      <motion.div ref={ref} whileTap={{scale: .95}} transition={{type: "spring"}} className="aboutMeDescriptionBorder">
+
+      {/* div to handle click spring animation */}
+      <motion.div ref={ref} whileTap={{scale: .95}} onClick={moreInfo} transition={{type: "spring"}} className="aboutMeDescriptionBorder">
+        
         {/* Div to handle on click and its animations */}
-        <motion.div initial={{opacity:0}} onClick={moreInfo} whileHover={{opacity:1}} transition={{duration: 1}} className="animateHoverContent"></motion.div>
+        <motion.div initial={{opacity:0}} whileHover={{opacity:1}} transition={{duration: 1}} className="animateHoverContent"></motion.div>
+        
         {/* Div to handle on first view blur animation */}
         <motion.div initial={{width:"100%"}} animate={{width:isInView ? "0%":"100%"}} transition={{duration: 2}}  className="animateContent"></motion.div>
         <div className="aboutMeDescriptionTitle">{props.title}</div>
