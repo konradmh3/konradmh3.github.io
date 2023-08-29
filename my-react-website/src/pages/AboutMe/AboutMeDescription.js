226539a8d7706779f898f10a30d78615cf80f2df
konrad <konradm.h.3@gmail.com>
2023-08-29 00:40:35 -0700

    added animations for clicking buttons, going to focus on extendiong content for clickable items by adjusting height of invisible div containing the content in about me description border

--- my-react-website/src/pages/AboutMe/AboutMeDescription.js
+++ my-react-website/src/pages/AboutMe/AboutMeDescription.js
@@ -2,7 +2,7 @@
 // it will take in a subtitle and short desription as props
 import React from "react";
 import "../../style/AboutMe.css";
-import { useInView } from "framer-motion";
+import { spring, useInView } from "framer-motion";
 import { forwardRef } from "react";
 
 
@@ -10,23 +10,27 @@ import { motion } from "framer-motion";
 
 const AboutMeDescription = (props, ref) => {
   const isInView = useInView(ref, { once: true, amount: 1 });
+  var count = 0;
+
+
   
 // create more info function to handle the animation of the div
   const moreInfo = () => {
-    console.log("More info clicked!");
+    count++;
+    console.log("This button clicked", count, "times!");
   };
 
 
   return (
     <div className="aboutMeDescriptionContainer">
-      <div ref={ref} className="aboutMeDescriptionBorder">
+      <motion.div ref={ref} whileTap={{scale: .95}} transition={{type: "spring"}} className="aboutMeDescriptionBorder">
         {/* Div to handle on click and its animations */}
         <motion.div initial={{opacity:0}} onClick={moreInfo} whileHover={{opacity:1}} transition={{duration: 1}} className="animateHoverContent"></motion.div>
         {/* Div to handle on first view blur animation */}
         <motion.div initial={{width:"100%"}} animate={{width:isInView ? "0%":"100%"}} transition={{duration: 2}}  className="animateContent"></motion.div>
         <div className="aboutMeDescriptionTitle">{props.title}</div>
         <div className="aboutMeDescriptionText">{props.text}</div>
-      </div>
+      </motion.div>
     </div>
   );
 };
