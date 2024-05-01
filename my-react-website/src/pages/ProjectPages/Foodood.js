c264828ea7d766069b5304a73b86b96dd92e73a8
konradmh3 <konradm.h.3@gmail.com>
2024-05-01 13:39:39 -0700

    testing react router

--- my-react-website/src/pages/ProjectPages/Foodood.js
+++ my-react-website/src/pages/ProjectPages/Foodood.js
@@ -5,17 +5,17 @@ import { createBrowserHistory } from "history";
 
 
 const Foodood = () => {
-    const history = createBrowserHistory();
+    // const history = createBrowserHistory();
     // history.back();
     // the above is to test on safari with createBrowserHistory
     //the above works automatically when you join page but not when
     //you click the back button
     //the below works when you click the back button
-    useEffect(() => {
-        window.onpopstate = function(event) {
-            history.back();
-        }
-    }, [])
+    // useEffect(() => {
+    //     window.onpopstate = function(event) {
+    //         history.back();
+    //     }
+    // }, [])
 
     // Found a different solution then the above but might want to employ this solution and test which is better
 
