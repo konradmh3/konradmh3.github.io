c646360d1a1d042b38aa457679ee99918baadb8b
konradmh3 <konradm.h.3@gmail.com>
2024-05-20 19:58:06 -0700

    added descriptions for stick and jarvis

--- my-react-website/src/pages/ProjectPages/Jarvis.js
+++ my-react-website/src/pages/ProjectPages/Jarvis.js
@@ -1,15 +1,15 @@
 import "../../style/Jarvis.css";
-import { useEffect } from 'react';
-import { Container, Row, Col } from 'react-grid-system';
-import FirstScene from '../../scenes/FirstScene';
+import { useEffect } from "react";
+import { Container, Row, Col } from "react-grid-system";
+import FirstScene from "../../scenes/FirstScene";
 
 const Jarvis = () => {
-    useEffect(() => {
-        window.scrollTo(0, document.body.scrollHeight);
-      }, []);
-    return (
-        <div className='JarvisContainer'>
-                <div className="jarvisContainerNoScroll">
+  useEffect(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, []);
+  return (
+    <div className="JarvisContainer">
+      <div className="jarvisContainerNoScroll">
         <Container>
           <Row className="titleRowJarvis">
             <Col xs={12} className="titleColJarvis">
@@ -37,7 +37,19 @@ const Jarvis = () => {
           </Row>
           <Row className="descriptionRowJarvis">
             <Col xs={12} className="descriptionColJarvis">
-              <div className="descriptionContainerJarvis"></div>
+              <div className="descriptionContainerJarvis">
+                <b>Technology:</b> C++, Python, REST API <br></br>
+                <b>Description:</b> Jarvis is
+                a voice-controlled Home System and server, similar to Alexa,
+                engineered to control third-party and custom-made IOT devices
+                around my room and coded in python. <br></br>
+                <b>Implementation:</b> Designed and
+                implemented the speech recognition module and server to send
+                commands and interpret voice commands using a Python server,
+                allowing seamless interaction with the system. Used the esp32
+                module to create a low cost api server to send data and receive
+                commands for custom iot devices.
+              </div>
             </Col>
           </Row>
           <Row className="buttonRowJarvis">
@@ -51,7 +63,7 @@ const Jarvis = () => {
         </Container>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default Jarvis;
