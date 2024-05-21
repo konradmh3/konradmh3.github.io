7264bc451ae49fb49e859011cb63cc32cf217656
konradmh3 <konradm.h.3@gmail.com>
2024-05-20 19:13:28 -0700

    addded stick content

--- my-react-website/src/pages/ProjectPages/Stick.js
+++ my-react-website/src/pages/ProjectPages/Stick.js
@@ -13,41 +13,60 @@ const Stick = () => {
       <div className="stickContainer">
         <Container>
           <Row className="titleRowStick">
-            <Col xs={12} className="titleColStick">Stick</Col>
+            <Col xs={12} className="titleColStick">
+              Stick
+            </Col>
           </Row>
           <Row className="videoRowStick">
             <Col xs={12} className="videoColStick">
-                <div className="videoContainerStick"></div>
+              <div className="videoContainerStick">
+                <iframe
+                  className="ytplayer"
+                  type="text/html"
+                  title="stickPlayThroughClip"
+                  width="100%"
+                  src="https://www.youtube.com/embed/Ls3ZGHq0C3c?autoplay=1&controls=1&loop=5&color=white&playlist=Ls3ZGHq0C3c"
+                  frameborder="0"
+                  allowfullscreen
+                  allow="autoplay"
+                ></iframe>
+              </div>
             </Col>
           </Row>
           <Row className="frameWorksRowStick">
             <Col xs={3} className="frameWorksColStick">
-                <FirstScene />
+              <FirstScene />
             </Col>
             <Col xs={3} className="frameWorksColStick">
-            <FirstScene />
+              <FirstScene />
             </Col>
             <Col xs={3} className="frameWorksColStick">
-            <FirstScene />
+              <FirstScene />
             </Col>
             <Col xs={3} className="frameWorksColStick">
-            <FirstScene />
+              <FirstScene />
             </Col>
           </Row>
           <Row className="descriptionRowStick">
             <Col xs={12} className="descriptionColStick">
-                <div className="descriptionContainerStick"></div>
+              <div className="descriptionContainerStick">
+                <b>Technology:</b> Unity Engine, C#, Spine, Blender<br></br>
+                <b>Description:</b> 2D Platformer. Play through different levels with the goal of
+                getting to the door in each level before time runs out and
+                without dying. Inspiration from Mario and N Flash game.<br></br>
+                <b>Implementation:</b> Designed and implemented core gameplay
+                mechanics, including player movement, collision detection, and
+                scoring system. Integrated Spine for character animations,
+                enhancing visual appeal and interactivity.
+              </div>
             </Col>
           </Row>
           <Row className="buttonRowStick">
             <Col xs={6} className="githubColStick">
-                <div className="downloadContainerStick">
-                </div>
+              <div className="downloadContainerStick"></div>
             </Col>
             <Col xs={6} className="downloadColStick">
-                <div className="downloadContainerStick">
-
-                </div>
+              <div className="downloadContainerStick"></div>
             </Col>
           </Row>
         </Container>
